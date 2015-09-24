2fa7ca9c785a2bd9944f93287b295bf88e149cf0
Michal Hojgr <michal.hojgr@gmail.com>
2015-09-24 20:23:19 +0200

    Add comments to each type/function

--- web/client.go
+++ web/client.go
@@ -7,11 +7,13 @@ import (
 	"strconv"
 )
 
+// Client is HTTP interface that is customized for Travian
 type Client struct {
 	GoClient *http.Client
 	BaseURL  string
 }
 
+// NewClient creates new instance of Client struct
 func NewClient(baseURL string) *Client {
 	cookieJar, _ := cookiejar.New(nil)
 
@@ -22,10 +24,17 @@ func NewClient(baseURL string) *Client {
 	return &Client{client, baseURL}
 }
 
+// POST Creates POST request
 func (c *Client) POST(url string, vals url.Values) (resp *http.Response, err error) {
 	return c.GoClient.PostForm(url, vals)
 }
 
+// GET creates GET request
+func (c *Client) GET(url string) (resp *http.Response, err error) {
+	return c.GoClient.Get(url)
+}
+
+// Login logs client in
 func (c *Client) Login(username, password string) {
 	c.POST(c.BaseURL, url.Values{
 		"name": {username},
@@ -33,20 +42,19 @@ func (c *Client) Login(username, password string) {
 	})
 }
 
-func (c *Client) GET(url string) (resp *http.Response, err error) {
-	return c.GoClient.Get(url)
-}
-
+// GetStatisticsHTML Returns response from Statistics.php
 func (c *Client) GetStatisticsHTML() (*http.Response, error) {
 	resp, err := c.GoClient.Get(c.BaseURL + "/statistics.php")
 	return resp, err
 }
 
+// GetVillage1HTML Returns response from Village1.php
 func (c *Client) GetVillage1HTML() (*http.Response, error) {
 	resp, err := c.GoClient.Get(c.BaseURL + "/village1.php")
 	return resp, err
 }
 
+// GetCookie returns PHPSESSID
 func (c *Client) GetCookie() string {
 	url, _ := url.Parse("http://s5.zravian.com")
 	cookies := c.GoClient.Jar.Cookies(url)
@@ -60,6 +68,7 @@ func (c *Client) GetCookie() string {
 	return ""
 }
 
+// UpgradeField upgrades field to a higher level
 func (c *Client) UpgradeField(id int, key string) {
 	c.GoClient.Get(c.BaseURL + "/village1.php?id=" + strconv.Itoa(id) + "&k=" + key)
 }
